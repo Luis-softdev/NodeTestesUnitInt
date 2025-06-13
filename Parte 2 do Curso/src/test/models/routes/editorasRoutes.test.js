023dc3d41e3ce3f2fdcbf901e0f3998e957206d6
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2025-06-13 13:47:39 -0300

    feat: add validation for empty request body in cadastrarEditora method and update response status to 204 in atualizarEditora method; add tests for editoras routes including GET, POST, PUT, and DELETE operations

---
+++ Parte 2 do Curso/src/test/models/routes/editorasRoutes.test.js
@@ -0,0 +1,83 @@
+import {
+  afterEach, beforeEach, describe, expect, jest,
+} from '@jest/globals';
+import request from 'supertest';
+import app from '../../../app.js';
+
+let server;
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe('GET em /editoras', () => {
+  it('Deve retornar uma lista de editoras', async () => {
+    const resposta = await request(app)
+      .get('/editoras')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResposta;
+describe('POST em /editoras', () => {
+  it('Deve adicionar uma nova editora', async () => {
+    const resposta = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'São Paulo',
+        email: 's@s.com',
+      });
+    expect(201);
+
+    idResposta = resposta.body.content.id;
+  });
+
+  it('Deve não adicionar nada ao passar o body vazio', async () => {
+    await request(app)
+      .post('/editoras')
+      .send({})
+      .expect(400);
+  });
+});
+
+describe('GET em /editoras/id', () => {
+  it('Deve retornar o recurso adicionado', async () => {
+    await request(app)
+      .get(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
+
+describe('PUT em /editoras/id', () => {
+  it.each([
+    ['nome', { nome: 'CDC Editora' }],
+    ['cidade', { cidade: 'Rio de Janeiro' }],
+    ['email', { email: 'e@e.com' }],
+  ])('Deve alterar o campo %s da editora', async (param) => {
+    const requisicao = { request };
+    const spy = jest.spyOn(requisicao, 'request');
+    await requisicao.request(app)
+      .put(`/editoras/${idResposta}`)
+      .send(param)
+      .expect(204);
+
+    expect(spy).toHaveBeenCalledWith(app);
+  });
+});
+
+describe('DELETE em /editoras/id', () => {
+  it('Deve remover a editora adicionada', async () => {
+    await request(app)
+      .delete(`/editoras/${idResposta}`)
+      .expect(200);
+  });
+});
